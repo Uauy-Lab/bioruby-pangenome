e81f29f679361681e9f88a76b3f10f26f1e72821
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-05-01 20:26:39 +0100

    Starting edits for the mask

--- lib/bio-pangenome/sequence_hash.rb
+++ lib/bio-pangenome/sequence_hash.rb
@@ -2,4 +2,13 @@
  	def to_fasta( size: 60)
  		self.keys.map { |name, val|  self[name].to_fasta(name, size) }.join("")
  	end
+
+ 	def masks
+ 		
+ 	end
+
+ end
+
+ class Bio::PanGenome::MaskSite
+ 	attr_accessor :gap, :ambiguous, :snp 
  end
